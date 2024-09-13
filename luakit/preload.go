9feaf35c812615d0dd4c00d0fb41b5d6bab3b435
edunx <jianguo@eastmoney.com>
2024-09-13 19:37:33 +0800

    update integer

--- luakit/preload.go
+++ luakit/preload.go
@@ -11,6 +11,8 @@ func PreloadModule(L *lua.LState) {
 	kv := lua.NewUserKV()
 	kv.Set("map", lua.NewFunction(NewMapL))
 	kv.Set("slice", lua.NewFunction(NewSliceL))
+	kv.Set("int", lua.NewFunction(NewIntL))
+	kv.Set("pretty", lua.NewFunction(PrettyJsonL))
 	kv.Set("fmt", lua.NewFunction(NewFmtL))
 	kv.Set("pretty", lua.NewFunction(PrettyJsonL))
 	L.SetGlobal("luakit", kv)
