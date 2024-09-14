18ab4bd352d62eef3307eb26faf806182f360def
edunx <jianguo@eastmoney.com>
2024-09-14 18:36:44 +0800

    luakit api

--- cast/string.go
+++ cast/string.go
@@ -1,7 +1,6 @@
 package cast
 
 import (
-	"github.com/vela-public/lua"
 	"reflect"
 	"unsafe"
 )
@@ -17,12 +16,4 @@ func S2B(s string) (b []byte) {
 
 func B2S(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
-}
-
-func S2L(s string) lua.LString {
-	return lua.LString(s)
-}
-
-func B2L(b []byte) lua.LString {
-	return *(*lua.LString)(unsafe.Pointer(&b))
 }
